fca3de8454c55bd59d94fa2147d7adfcd21e7ef2
pete <pete@ubuntudev.(none)>
2014-08-14 18:33:27 -0500

    [added]Added OMP Parrallel example in C

---
+++ C/omptest.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <omp.h>
+
+int main() {
+	int nthreads, tid;
+
+	printf("Hello");
+	#pragma omp parallel private(tid)
+	{
+		tid = omp_get_thread_num();
+		printf("Hello from thread %d\n",tid);
+		if ( tid == 0 ) {
+			nthreads = omp_get_num_threads();
+			printf("num threads is %d\n",nthreads);
+		}
+
+	}
+}
