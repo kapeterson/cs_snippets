58126275bfc8c2fb296c7a66a348400702810936
elpete <kapeterson@gmail.com>
2014-12-28 13:14:27 -0600

    added file parameter to the test wireshark program

---
+++ cpp/Wireshark/test.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <stdio.h>
+#include <pcap.h>
+#include <stdlib.h>
+#include <string>
+#define ETHER_ADDR_LEN 6
+
+// compile with g++ -I/usr/lib/pcap test.cpp -lpcap
+
+
+using namespace std;
+
+
+struct sniff_ethernet {
+	u_char ether_dhost[ETHER_ADDR_LEN];
+	u_char ether_shost[ETHER_ADDR_LEN];
+	u_short ether_type;
+};
+
+void printUsage(){
+	printf("Usage: test.cpp <filename>\n\n");
+}
+int main(int argc, char *argv[]){
+
+	if ( argc < 2 || argc > 3 ) {
+		printUsage();
+		return 1;
+	}
+
+	// default is /home/pete/temp/capture/.pcap
+
+	string file = argv[1];
+	cout << "File is " << file << "\n";
+	
+	char errbuff[PCAP_ERRBUF_SIZE];
+
+	pcap_t* pcap = pcap_open_offline(file.c_str(), errbuff);
+	struct pcap_pkthdr *header;
+	const struct sniff_ethernet *ethernet;
+
+	const struct sniff_ip *ip;
+	const u_char *data;
+
+	u_int size_ip;
+	int packetcount = 0;
+
+	while ( int returnValue = pcap_next_ex(pcap, &header, &data ) >= 0 ){
+		printf( "packet %d\n", packetcount);
+
+		ethernet  = (struct sniff_ethernet*)data;
+	//	ip = (struct sniff_ip*)(data + SIZE_ETHERNET);
+	//	cout << "SRC IP is " <<  ethernet->ether_shost << "\n";
+		packetcount++;
+	}
+	return 0;
+}
