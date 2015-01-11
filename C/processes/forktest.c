840d7aab83c90ff227590da1c4dcd2e162af21b7
Pete <pete@Vader.(none)>
2015-01-11 10:52:09 -0600

    added a simple for example

---
+++ C/processes/forktest.c
@@ -0,0 +1,27 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(){
+
+	//int pid;
+	pid_t  pid = fork();
+	
+	//printf("The pid is %d\n", (int)pid);
+	if ( pid < 0 ) {
+		fprintf(stderr, "Fork Failed");
+		return 1;
+	} else if ( pid == 0  ) {
+		printf("We good yall %d\n", (int)pid);
+		execlp("/bin/ls", "ls", NULL);
+	} else {
+		
+		printf("Original thread gonna wait\n");
+		wait(NULL);
+		printf("Chile done yo\n");
+
+	}
+
+	printf("OK\n");
+	return 0;	
+}
