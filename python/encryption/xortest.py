9365cd61e50de636a3c7725c76d2c95f0e8aebbe
pete <kapeterson@gmail.com>
2015-10-17 14:28:30 -0500

    {added]Added simple python encryption test

---
+++ python/encryption/xortest.py
@@ -0,0 +1,30 @@
+x = 0
+y = 0
+z = 1
+
+
+print "x^y = " + str( x ^ y)
+print "x^z = " + str(x ^ z ) 
+
+
+key =   "aabbcc"
+ctext = "tester"
+cipher = ""
+
+for i in range(len(ctext)):
+	print ctext[i]
+	cipher += chr(ord(ctext[i]) ^ ord(key[i]))
+
+
+print "Cipher is " + cipher
+
+
+
+ct = ""
+for i in range(len(ctext)):
+	ct += chr(ord(cipher[i]) ^ ord(key[i]))
+
+
+
+
+print "Decrypt is " + ct
